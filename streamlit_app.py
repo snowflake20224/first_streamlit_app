0ed1cbda63a94ca2256a6c9a7f988a1d9e2751b4
snowflake20224 <104080377+snowflake20224@users.noreply.github.com>
2022-04-20 14:52:22 +0100

    Update streamlit_app.py
    add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,8 @@ import streamlit
 streamlit.title("My new streamlit App!")
 streamlit.header("Some of the things to do")
 streamlit.text("Learn snowflake")
-
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
