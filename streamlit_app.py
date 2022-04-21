02f1c433f645b8d8595ac88724ab216b66c7e3c0
snowflake20224 <104080377+snowflake20224@users.noreply.github.com>
2022-04-21 10:29:57 +0100

    Update streamlit_app.py
    add table

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,7 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())  
+
+fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
